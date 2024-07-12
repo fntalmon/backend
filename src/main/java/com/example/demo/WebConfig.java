0af1bb2a0fcc86c220cf1c491b84e1d0a36ce03d
fntalmon <54511081+Federicotalmon@users.noreply.github.com>
2024-07-12 16:17:42 -0300

    Permitia como dominio al proyecto de Springboot, no al de React!

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://renderproject-wl0t.onrender.com")
+                .allowedOrigins("http://localhost:3000","https://frontendnotes-7wfn.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
